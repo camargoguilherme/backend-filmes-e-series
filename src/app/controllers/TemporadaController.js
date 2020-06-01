5742f390fead41712c9a9bcf4e826527d104ae62
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-05-31 21:01:25 -0300

    Alterado o tipo de retorno das temporadas

--- src/app/controllers/TemporadaController.js
+++ src/app/controllers/TemporadaController.js
@@ -3,21 +3,36 @@ import Temporada from '../models/Temporada';
 class TemporadaController {
   // Retrieve and return all temporadas from the database.
   async find(req, res) {
-    Temporada.find(
-      {...req.query},
-      {_id: 1, title: 1, temporadas: 1, serie: 1, episodios: 1}
-    )
-      .populate('episodios')
-      .exec()
-      .then((temporadas) => {
-        return res.send(temporadas);
-      })
-      .catch((err) => {
-        return res.status(500).send({
-          message:
-            err.message || 'Some error occurred while retrieving temporadas.',
-        });
+    console.log(req.query);
+    try {
+      const temporadas = await Temporada.find(
+        {...req.query},
+        {_id: 1, title: 1, temporadas: 1, serie: 1, episodios: 1}
+      )
+        .populate('episodios')
+        .exec();
+      // return res.send(temporadas);
+      return res.send(
+        temporadas.map((temporada) => {
+          const dublado = [];
+          const legendado = [];
+          temporada.episodios.forEach((episodio) => {
+            if (episodio.dublado) {
+              dublado.push(episodio);
+            } else {
+              legendado.push(episodio);
+            }
+          });
+          temporada.episodios = {dublado, legendado};
+          return temporada;
+        })
+      );
+    } catch (error) {
+      return res.status(500).send({
+        message:
+          error.message || 'Some error occurred while retrieving temporadas.',
       });
+    }
   }
 }
 
